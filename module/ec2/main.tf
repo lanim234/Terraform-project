f482dd259c9530ac0be25abb66b1b069aefd0f9e
Smith <smith@hdudhhd.com>
2023-06-08 16:47:24 -0500

    commit

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name = "sample"
+    Name = var.name
   }
 }
 
@@ -14,7 +14,7 @@ data "aws_ami" "example" {
 }
 
 resource "aws_security_group" "sg" {
-  name        = "sample"
+  name        = var.name
   description = "Allow TLS inbound traffic"
 
 
@@ -35,6 +35,10 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = "sample"
+    Name = var.name
   }
+}
+
+variable "name" {
+
 }
