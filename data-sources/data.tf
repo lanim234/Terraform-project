53003c35f2aca75d3e9ce5ecf14ed423baca9dc4
Smith <smith@hdudhhd.com>
2023-06-07 20:45:04 -0500

    commit

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -23,7 +23,7 @@ output "sgid" {
 data "aws_ami" "example" {
   owners = ["973714476881"]
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-practice"
+  name_regex       = "Centos-8-DevOps-Practice"
 }
  output "ami" {
    value = data.aws_ami.example.id
