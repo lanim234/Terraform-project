288ccb12211ee7f4113d09e2abd54a26a6097996
Smith <smith@hdudhhd.com>
2023-06-08 15:37:58 -0500

    commit

--- resource/main.tf
+++ resource/main.tf
@@ -1,4 +1,3 @@
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
@@ -6,4 +5,10 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+data "aws_ami" "example" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
 }
