7ca9d9bc3951f2715fe2d7249c47a4418cefda58
Smith <smith@hdudhhd.com>
2023-06-09 18:08:18 -0500

    commit

--- functions/main.tf
+++ functions/main.tf
@@ -28,5 +28,9 @@ variable "classes" {
 }
 
 output "devops_topics" {
-  value =var.classes["devops"]["topics"]
+  value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics" {
+  value = var.classes["aws"]["topics"]
 }
